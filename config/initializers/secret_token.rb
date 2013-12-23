5d935f218bbfc89cbcc5d4274e9858118aa0c250
Joe McLean <mclean.joe@gmail.com>
2013-12-23 17:30:56 -0500

    First commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DannyBoy::Application.config.secret_key_base = '63d5266e9c55a6b5bc105ce55b69dbfd9f9274c8b486053bcc42ce0cb22c0d6382ebfe60b4c05961c28a3b31c27a1ca0c5bee3258c23f961aee8fad6d453de0c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DannyBoy::Application.config.session_store :cookie_store, key: '_danny_boy_session'
