dc3da461ed3dc4f9ab7fa8291b8ff0510d5baeb4
Joe McLean <mclean.joe@gmail.com>
2013-12-24 14:36:43 -0500

    image uploading working on heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131224001829) do
+ActiveRecord::Schema.define(version: 20131224183658) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20131224001829) do
     t.string   "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
